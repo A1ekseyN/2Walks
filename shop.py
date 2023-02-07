abc70d6d19f6c7a2391994fcdc36529d24110498
A1ekseyN <lolik4ever@rambler.ru>
2023-02-07 16:32:03 +0200

    Version: 0.0.1s Date: 07.02.2023 - Add Clothes menu in magazine - Add shoes buy option

--- shop.py
+++ shop.py
@@ -17,14 +17,14 @@ class Shop():
             'price': [],
         }
 
-        print('\n--- 🛒 Вы находитесь в локации - Магазин --- 🛒'
+        print('\n--- 🛒 Магазин --- 🛒'
               '\nВ этой локации, Вы можете приобрести разное оборудование, и расходные материалы.'
               f'\n{money}')
         print('\nВы можете приобрести: '
               '\n\t1. Еда, вода, расходные материалы'
               '\n\t2. Одежда (Не работает)'
-              '\n\t3. Экипировка (Не работает)'
-              '\n\t9. Продать товар (Не работает)'
+#              '\n\t3. Экипировка (Не работает)'
+#              '\n\t9. Продать товар (Не работает)'
               '\n\t0. Назад')
         try:
             ask = input('\nВыберите раздел меню: \n>>> ')
@@ -32,7 +32,8 @@ class Shop():
                 Shop.shop_menu_food_and_water(self, item=item, money=money)
                 Shop.shop_menu(self)
             elif ask == '2':
-                pass
+                Shop.shop_menu_clothes(self, item=item, money=money)
+                Shop.shop_menu(self)
             elif ask == '3':
                 pass
             elif ask == '9':
@@ -95,9 +96,128 @@ class Shop():
             Shop.shop_menu_food_and_water(self, item, money)
 
 
-    def shop_menu_clothes(self):
+    def shop_menu_clothes(self, item, money):
         # Раздел для покупки Одежды.
-        pass
+        def clothes_head(self, money):
+            print('\nВ этом меню можно приобрести головной убор: '
+                  f'\n{money}'
+                  '\n1. ---'
+                  '\n2. ---'
+                  '\n3. ---'
+                  '\n0. Назад')
+            try:
+                ask = input('\nЧто вы хотите приобрести? \n>>> ')
+                if ask == '1':
+                    print(' --- Тестовая покупка')
+                elif ask == '0':
+                    Shop.shop_menu_clothes(self, item, money)
+                else:
+                    clothes_head(self, money)
+            except:
+                clothes_head(self, money)
+
+        def clothes_jacket(self, money):
+            print('\nВ этом меню можно приобрести куртку: '
+                  f'\n{money}')
+
+        def clothes_pants(self, money):
+            print('\nВ этом меню можно приобрести штаны: '
+                  f'\n{money}')
+
+        def clothes_gloves(self, money):
+            print('\nВ этом меню можно приобрести перчатки: '
+                  f'\n{money}')
+
+        def clothes_shoes(self, item, money):
+            print('\nВ этом меню можно приобрести обувь: '
+                  f'\n{money}'
+                  f'\n\t1. Кеды - C-Grade (+ 1 % шагов) (Цена: 25 $)'
+                  f'\n\t2. Кеды - B-Grade (+ 2 % шагов) (Цена: 50 $)'
+                  f'\n\t3. Кеды - A-Grade (+ 3 % шагов) (Цена: 100 $)'
+                  f'\n\t0. Назад')
+            try:
+                ask = input('\nЧто вы хотите приобрести? \n>>> ')
+                if ask == '1':
+                    if char_characteristic['money'] >= 25:
+                        item['item_name'].append('Кеды')
+                        item['item_type'].append('shoes')
+                        item['grade'].append('C-Grade')
+                        item['characteristic'].append('steps')
+                        item['bonus'].append(1)
+                        item['quality'].append(100)
+                        item['price'].append(25)
+                        char_characteristic['inventory'].append(item)
+                        char_characteristic['money'] -= 25
+                        print('\nВы приобрели: Кеды - C-Grade (+ 1 % шагов) за - 25 $.')
+                    else:
+                        print(f'\nУ вас не достаточно денег. Не хватает 💰: {25 - char_characteristic["money"]} $.')
+                elif ask == '2':
+                    if char_characteristic['money'] >= 50:
+                        item['item_name'].append('Кеды')
+                        item['item_type'].append('shoes')
+                        item['grade'].append('B-Grade')
+                        item['characteristic'].append('steps')
+                        item['bonus'].append(2)
+                        item['quality'].append(100)
+                        item['price'].append(50)
+                        char_characteristic['inventory'].append(item)
+                        char_characteristic['money'] -= 50
+                        print('\nВы приобрели: Кеды - B-Grade (+ 2 % шагов) за - 50 $.')
+                    else:
+                        print(f'\nУ вас не достаточно денег. Не хватает 💰: {50 - char_characteristic["money"]} $.')
+                elif ask == '3':
+                    if char_characteristic['money'] >= 100:
+                        item['item_name'].append('Кеды')
+                        item['item_type'].append('shoes')
+                        item['grade'].append('A-Grade')
+                        item['characteristic'].append('steps')
+                        item['bonus'].append(3)
+                        item['quality'].append(100)
+                        item['price'].append(100)
+                        char_characteristic['inventory'].append(item)
+                        char_characteristic['money'] -= 100
+                        print('\nВы приобрели: Кеды - A-Grade (+ 3 % шагов) за - 100 $.')
+                    else:
+                        print(f'\nУ вас не достаточно денег. Не хватает 💰: {100 - char_characteristic["money"]} $.')
+                elif ask == '0':
+                    Shop.shop_menu_clothes(self, item, money)
+                else:
+                    Shop.shop_menu_clothes(self, item, money)
+            except:
+                clothes_shoes(self, item, money)
+
+        print('\nВ можете купить одежду для персонажа.'
+            f'\n{money}'
+            '\n\t1. Шапка'
+            '\n\t2. Куртка'
+            '\n\t3. Штаны'
+            '\n\t4. Перчатки'
+            '\n\t5. Обувь'
+            '\n\t0. Назад')
+        try:
+            ask = input('\nВыберите раздел товара, который хотите приобрести: \n>>> ')
+            if ask == '1':
+                clothes_head(self, money)
+            elif ask == '2':
+                clothes_jacket(self, money)
+            elif ask == '3':
+                clothes_pants(self, money)
+            elif ask == '4':
+                clothes_gloves(self, money)
+            elif ask == '5':
+                clothes_shoes(self, item, money)
+                Shop.shop_menu_clothes(self, item, money)
+            elif ask == '0':
+                pass
+#                Shop.shop_menu(self)
+#                Shop.shop_menu_clothes(self, item, money)
+            else:
+                Shop.shop_menu_clothes(self, item, money)
+#            Shop.shop_menu_clothes(self, item, money)
+        except:
+            print('Выберите пункт меню одежды.')
+            Shop.shop_menu_clothes(self, item, money)
+
 
     def shop_menu_equipment(self):
         # Раздел для покупки экипировки.
